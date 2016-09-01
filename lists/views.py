54155a6441a5944d787c3a87ab2f602f35b3b723
rolight <flsnnx@gmail.com>
2016-09-01 03:58:33 -0400

    Create Model List

--- lists/views.py
+++ lists/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.utils.html_parser import HTMLParseError
 
 # Create your views here.
-from lists.models import Item
+from lists.models import Item, List
 
 def home_page(request):
     return render(request, 'lists/home.html')
@@ -17,6 +17,7 @@ def view_list(request):
     )
 
 def new_list(request):
+    list_ = List.objects.create()
     new_item_text = request.POST['item_text']
-    Item.objects.create(text=new_item_text)
+    Item.objects.create(text=new_item_text, list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
