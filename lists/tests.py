5e835242b932ed197fba143f90f2fc0e5de4882c
rolight <flsnnx@gmail.com>
2016-09-01 01:46:40 -0400

    Redirct after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -9,64 +9,88 @@ from lists.models import Item
 import re
 
 def remove_csrf_token(content):
-	p = re.compile("<.*?name='csrfmiddlewaretoken'.*?>")
-	return re.sub(p, '', content)
+    p = re.compile("<.*?name='csrfmiddlewaretoken'.*?>")
+    return re.sub(p, '', content)
 
-	
+    
 def none_empty_ch(content):
-	p = re.compile('\s')
-	return re.sub(p, '', content)
+    p = re.compile('\s')
+    return re.sub(p, '', content)
 
 def pure(content):
-	return none_empty_ch(remove_csrf_token(content))
+    return none_empty_ch(remove_csrf_token(content))
 
 class HomePageTest(TestCase):
-	
-	def test_root_url_resolves_to_home_page_view(self):
-		found = resolve('/')
-		self.assertEqual(found.func, home_page)
+    
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
 
-	def test_home_page_returns_correct_html(self):
-		request = HttpRequest()
-		response = home_page(request)
-		# We shouldn't test const varible
-		expected_html = render_to_string('lists/home.html')
-		response_content = remove_csrf_token(response.content.decode())
-		self.assertEqual(
-			none_empty_ch(response_content),
-			none_empty_ch(expected_html),
-			'\nexpect:\n%s\nbut:\n%s\n' % (expected_html, response_content)
-		)
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        # We shouldn't test const varible
+        expected_html = render_to_string('lists/home.html')
+        response_content = remove_csrf_token(response.content.decode())
+        self.assertEqual(
+            none_empty_ch(response_content),
+            none_empty_ch(expected_html),
+            '\nexpect:\n%s\nbut:\n%s\n' % (expected_html, response_content)
+        )
+    
+    def test_home_page_only_saves_items_when_necessary(self):
+        request = HttpRequest()
+        home_page(request)
+        self.assertEqual(Item.objects.count(), 0)
 
-	def test_home_page_can_save_a_POST_request(self):
-		request = HttpRequest()
-		request.method = 'POST'
-		request.POST['item_text'] = 'A new list item'
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
 
-		response = home_page(request)
-		self.assertIn('A new list item', response.content.decode())
+        response = home_page(request)
 
-		expected_html = render_to_string(
-			'lists/home.html',
-			{'new_item_text': 'A new list item'}
-		)
-		self.assertEqual(pure(response.content.decode()), pure(expected_html))
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_home_page_redirects_after_POST(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        response = home_page(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_home_page_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        request = HttpRequest()
+        response = home_page(request)
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
+
 
 class ItemModelTest(TestCase):
-	
-	def test_saving_and_retrieving_items(self):
-		first_item = Item()
-		first_item.text = 'The first (ever) list item'
-		first_item.save()
+    
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
 
-		second_item = Item()
-		second_item.text = 'Item the second'
-		second_item.save()
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
 
-		saved_items = Item.objects.all()
-		self.assertEqual(saved_items.count(), 2)
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
 
-		first_saved_item = saved_items[0]
-		second_saved_item = saved_items[1]
-		self.assertEqual(first_saved_item.text, first_item.text)
-		self.assertEqual(second_saved_item.text, second_item.text)
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, first_item.text)
+        self.assertEqual(second_saved_item.text, second_item.text)
+
+
